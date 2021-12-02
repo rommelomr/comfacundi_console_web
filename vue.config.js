773f927e071552076a6845487f1eeb2b502b70fc
Rommel Montoya <rommelmontoya97@gmail.com>
2021-12-02 11:39:50 -0500

    module exports fixed

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "comfacundi_console_api" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "https://rommelomr.github.io/comfacundi_console_web/" : "/"
 };
